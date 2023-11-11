7f61f2d32eba6155a3cac661cc86ebf6f7553767
Sampurna Pandit <sampurnapandit3@gmail.com>
2023-11-11 10:19:46 +0530

    first commit

---
+++ Frontend/src/app/services/user.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  url = environment.apiUrl;
+  constructor(private httpClient:HttpClient) {}
+  
+  signup(data:any){
+    return this.httpClient.post(this.url + "/user/signup" , data , {
+      headers:new HttpHeaders().set('Content-Type' , 'application/json')
+    })
+  }
+
+  forgotPassword(data:any){
+    return this.httpClient.post(this.url + "/user/forgot-password" , data , {
+      headers:new HttpHeaders().set('Content-Type' , 'application/json')
+    })
+  }
+  login(data:any){
+    return this.httpClient.post(this.url + "/user/login" , data , {
+      headers:new HttpHeaders().set('Content-Type' , 'application/json')
+    })
+  }
+  checkToken() {
+    return this.httpClient.get(this.url + "/user/check-token");
+  }
+
+  changePassword(data:any){
+    return this.httpClient.post(this.url + "/user/change-password" , data , {
+      headers:new HttpHeaders().set('Content-Type' , 'application/json')
+    })
+  }
+
+  getUsers(){
+    return this.httpClient.get(this.url + "/user/get");
+  }
+
+  update(data:any){
+    return this.httpClient.post(this.url + "/user/update" , data , {
+      headers:new HttpHeaders().set('Content-Type' , 'application/json')
+    })
+  }
+ 
+}
